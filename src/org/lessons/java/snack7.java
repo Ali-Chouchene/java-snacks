426fc993f0449333e0d864c86f874580c91dc39c
Ali-Chouchene <choucheneali9801@gmail.com>
2023-05-05 16:00:41 +0200

    new update super fix

--- src/org/lessons/java/snack7.java
+++ src/org/lessons/java/snack7.java
@@ -1,14 +1,18 @@
 package org.lessons.java;
 
+import java.util.Random;
+
 public class snack7 {
 	public static void main(String[] args) {
-		int num = 0;
-		do {
-			 double doubleRandomNumber1 = Math.random() * 100;
-			 int ranNum = (int)doubleRandomNumber1;
-			 num = ranNum;
-			 System.out.println(ranNum);
-		}while((num % 15) == 0);
-		System.out.println(num);
+		Random rand = new Random();
+		while(true) {
+			int ranNum = rand.nextInt(101);
+			System.out.println(ranNum);
+			if(ranNum % 15 == 0) {
+				return;
+			}
+			 
+		}
+		
 	}
 }
