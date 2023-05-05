0509daeb639014e54d33fc3f477447fc299b88dc
Ali-Chouchene <choucheneali9801@gmail.com>
2023-05-05 10:49:05 +0200

    new update super fix

--- src/org/lessons/java/snack1.java
+++ src/org/lessons/java/snack1.java
@@ -1,10 +1,20 @@
 package org.lessons.java;
 
+import java.util.Scanner;
+
 public class snack1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner input = new Scanner(System.in);
+		System.out.println("Digita un numero");
+		String numberS = input.nextLine();
+		input.close();
+		int number = Integer.parseInt(numberS);
+		if((number % 2) == 0) {
+			System.out.println("il numero " +number + " è pari");
+		}else {
+			System.out.println("il numero pari sucessivo "+ (number + 1 ));
+		}
 	}
 
 }
